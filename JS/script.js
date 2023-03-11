9eb0cc5d5ce9ad96cfb467a2156458a173e0146e
vaibhav6900 <starryvaibh786@gmail.com>
2023-03-11 23:16:21 +0530

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+var count=0;
+// document.getElementById("counter").innerHTML=count;
+function increment(){
+    count++;
+    document.getElementById("counter").innerHTML=count;
+}
+function decrement(){
+    count--;
+    document.getElementById("counter").innerHTML=count;
+}
